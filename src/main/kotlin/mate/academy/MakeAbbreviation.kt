a2135ab23f3a9f21996809dc20c85fd4b49d8212
Bohdan Vorobiei <vbs@ukr.net>
2025-10-29 20:17:42 +0200

    task

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -2,5 +2,11 @@ package mate.academy
 
 fun makeAbbr(source: String) : String {
     // implement this function
-    return ""
+    return source
+        .split(" ")
+        .filter { it.isNotEmpty() }
+        .map { it[0].uppercase() }
+        .joinToString("")
 }
+
+
